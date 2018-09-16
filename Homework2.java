96f4b006a388c2e76716922d72138b40059b7210
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-09-16 15:54:16 -0400

    Working solution

--- Homework2.java
+++ Homework2.java
@@ -10,7 +10,10 @@ public class Homework2 {
 	 * It takes in one parameter: a String
 	 * Save the string as a private instance variable
 	 */
-
+	 private String str;
+	 public Homework2(String s) {
+		 str = s;
+	 }
 
 	/* Write the method upperase
 	 * It does not take in any parameters
@@ -19,7 +22,9 @@ public class Homework2 {
 	 * uppercase letters and return the result
 	 * Hint: Search the String class for 'uppercase'
 	 */
-
+	 public String uppercase() {
+		 return str.toUpperCase();
+	 }
 
 	/* Write the method strip
 	 * It takes in one paramter: a String
@@ -28,17 +33,23 @@ public class Homework2 {
 	 * It should remove spaces, commas, periods, and exclamation points.
 	 * Hint: Search the String class for 'replace'
 	 */
-
+	 public String strip(String s) {
+		 s = s.replace(" ", "");
+		 s = s.replace(",", "");
+		 s = s.replace(".", "");
+		 s = s.replace("!", "");
+		 return s;
+	 }
 
 	/* Fix the errors in the method problem4
 	 */
 	public Rectangle problems() {
-		Rectangle r1 = (5, 10, 15, 20);
-		double width = Rectangle(5, 10, 15, 20).getWidth();
-		Rectangle r2;
+		Rectangle r1 = new Rectangle(5, 10, 15, 20);
+		double width = new Rectangle(5, 10, 15, 20).getWidth();
+		Rectangle r2 = new Rectangle();
 		r2.translate(15, 25);
-		r3 = new Rectangle();
-		r3.translate("Far, far away");
+		Rectangle r3 = new Rectangle();
+		r3.translate(10, 20);
 		return r3;
 	}
 
@@ -47,7 +58,7 @@ public class Homework2 {
 	 * parameter Rectangle
 	 */
 	public double getPerimeter(Rectangle rect) {
-
+		return rect.getWidth() * 2 + rect.getHeight() * 2;
 	}
 
 }
